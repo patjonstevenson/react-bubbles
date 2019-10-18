f3698f4cf0b06aa00eff7bac06be024acf80332d
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-18 09:11:17 -0700

    set up GET request in BubblePage, sets colorList successfully

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utilities/axiosWithAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -8,6 +8,15 @@ const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/colors")
+      .then(res => {
+        console.log("Successful GET request to colors: ", res);
+        setColorList(res.data);
+      })
+      .catch(err => console.log("Error getting colors: ", err));
+  }, [])
 
   return (
     <>
